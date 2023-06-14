c26bb6aa6426676eecbd749155d906d80c5dcae5
spences10 <spences10apps@gmail.com>
2023-06-14 19:43:13 +0100

    refactor: :recycle: move styles out to index page
    remove layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
+	import '../app.postcss'
 	import Slides from './lib/deck/slides.svelte'
 </script>
 
 <Slides />
+
+<style>
+	:global(:root) {
+		--r-main-font: 'Poppins';
+		--r-heading-font: 'Manrope';
+		--r-code-font: 'Victor Mono';
+		--r-heading-text-transform: none;
+	}
+</style>
