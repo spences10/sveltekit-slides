a1f8b5cb9dfa00f85401d82fbf02e89213724b00
spences10 <spences10apps@gmail.com>
2023-06-14 19:16:14 +0100

    chore: :art: run prettier

--- src/routes/lib/deck/slide.svelte
+++ src/routes/lib/deck/slide.svelte
@@ -1,14 +1,13 @@
-<script lang='ts'>
-  export let id: string | null = null
-  export let animate=false
-  export let restart=false
-
+<script lang="ts">
+	export let id: string | null = null
+	export let animate = false
+	export let restart = false
 </script>
 
 <section
-  data-autoanimate-id={id}
-  data-autoanimate={animate || null}
-  data-autoanimate-restart={restart || null}
+	data-autoanimate-id={id}
+	data-autoanimate={animate || null}
+	data-autoanimate-restart={restart || null}
 >
-  <slot/>
+	<slot />
 </section>
